95482555b45f5a7e8ff6d4652155024a716dbb07
math-nao <math.nao.dev@gmail.com>
2025-09-20 20:21:22 +0200

    Rollback on certificats secret type

--- scripts/certs.sh
+++ scripts/certs.sh
@@ -651,7 +651,6 @@ add_certs_to_secret() {
 
   secret_json='{}'
   secret_json=$(echo "${secret_json}" | jq --arg kind "Secret" '. + {kind: $kind}')
-  secret_json=$(echo "${secret_json}" | jq --arg type "kubernetes.io/tls" '. + {type: $type }')
   secret_json=$(echo "${secret_json}" | jq --arg name "${CERTS_SECRET_NAME}" '. + {metadata: { name: $name }}')
   secret_json=$(echo "${secret_json}" | jq '. + {data: {}}')
   secret_json=$(echo "${secret_json}" | jq --arg cacert "$(get_file_data_for_secret_json "${ACME_CA_FILE}")" '. * {data: {"ca.crt": $cacert}}')
@@ -664,6 +663,20 @@ add_certs_to_secret() {
   status_code_checker=$(k8s_api_call "GET" "/api/v1/namespaces/${cert_namespace}/secrets/${CERTS_SECRET_NAME}" 2>"${res_file}")
 
   debug "Status code checker: ${status_code_checker}"
+  
+  secret_type=$(cat "${res_file}" | jq -r '.type')
+  if [ "${status_code_checker}" = "200" ] && [ "${secret_type}" = "kubernetes.io/tls" ]; then
+    info "Secret has been found and it is a kubernetes.io/tls type. Need to remove it due to issue introduced in certs 2.1.2 (kubernetes.io/tls type secrets are immutable; use opaque type secrets because patch is allowed)"
+    
+    status_code_delete=$(k8s_api_call "DELETE" "/api/v1/namespaces/${cert_namespace}/secrets" 2>/dev/null)
+    
+    debug "Status code delete: ${status_code_delete}"
+    
+    if [ "${status_code_delete}" = "200" ]; then
+      debug "set status code checker to 404 to force adding certs"
+      status_code_checker="404"
+    fi
+  fi
 
   status_code=""
   if [ "${status_code_checker}" != "200" ]; then
